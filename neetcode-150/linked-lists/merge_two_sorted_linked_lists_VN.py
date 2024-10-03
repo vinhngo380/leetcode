6623c5e63e02c437a77a0b79fbfa4353feb50f80
Vinh Ngo <vinhngo380@gmail.com>
2024-10-03 12:46:32 -0400

    7 -- Easy (kinda)
    i did this problem a long time ago

---
+++ neetcode-150/linked-lists/merge_two_sorted_linked_lists_VN.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummyHead = ListNode()
+        result = dummyHead
+        while list1 and list2:
+            if list1.val <= list2.val:
+                result.next = ListNode(list1.val)
+                list1 = list1.next
+            elif list2.val < list1.val:
+                result.next = ListNode(list2.val)
+                list2 = list2.next
+            result = result.next
+
+        if list1:
+            result.next = list1   
+        if list2:
+            result.next = list2   
+        return dummyHead.next
+
+'''TLDR
+make a resulting LL then:
+    look at the first value of each LL, whichever one is less
+    add it to the resulting LL and only move that list
+    keep going until one of the LL run out
+
+at the very end, add the other LL if there are remaining elements 
+'''
+
