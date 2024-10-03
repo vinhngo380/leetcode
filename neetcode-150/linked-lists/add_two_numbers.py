cab43d70e0b8bfaddb52066f7586aeb375916fdb
Jon Chen <82542510+thisisjonchen@users.noreply.github.com>
2024-10-03 12:53:24 -0400

    10 (Med) => 30

---
+++ neetcode-150/linked-lists/add_two_numbers.py
@@ -0,0 +1,40 @@
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        multiplier = 1
+        num1, num2 = 0, 0
+        while l1 is not None: # TC O(n)
+            num1 += l1.val * multiplier
+            multiplier *= 10
+            l1 = l1.next
+        multiplier = 1
+        while l2 is not None: # TC O(m)
+            num2 += l2.val * multiplier
+            multiplier *= 10
+            l2 = l2.next
+        
+        total = num1 + num2
+
+        # get digits
+        stack = [] # SC O(9 * 100) = O (900) = O(1), TC O(100) = O(1)
+        while total >= 0: # >= to allow for only 0 to be added
+            stack.append(total % 10)
+            total //= 10
+            if total == 0:
+                break
+
+        stack.reverse() # digits should now be in the right order to be added to LL, TC O(n)
+
+        dummy = root = ListNode()
+        while stack: # TC O((n+m)%10) < O(n) <= O(m)
+            digit = stack.pop()
+            root.next = ListNode(digit)
+            root = root.next
+        
+        return dummy.next
+
+  '''
+  TLDR: Iter through both LL, get sum, then use % and // with stack to get individual digits. Then, use the stack to create a new LL.
+
+  TC O(n+m)
+  SC O(1)
+  '''
