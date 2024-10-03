54276f7038851acf908ea58b4e40eb5f4ed44e78
Jon Chen <82542510+thisisjonchen@users.noreply.github.com>
2024-10-03 16:37:15 -0400

    10 (Easy)

---
+++ neetcode-150/linked-lists/linked_list_cycle_JC.py
@@ -0,0 +1,18 @@
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # Tortoise + Hare Situation
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+
+
+'''
+TLDR: Tortoise + Hare, use slow + fast pointers. When fast eventually catches up to slow, and they equal, then return True. If fast or fast.next == None, then that means there is an end (no cycle).
+
+TC O(n)
+SC O(1)
