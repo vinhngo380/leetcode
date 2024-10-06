5795f55fa25ee3f47b4fb178dccdd3e23ce1cdcd
Micah <141525046+Micah779@users.noreply.github.com>
2024-10-06 14:24:34 -0500

    6 (Medium)

---
+++ neetcode-150/linked-lists/reorder_list_MA.java
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        ListNode mid = findMid(head);
+        ListNode reversed = reverse(mid);
+        merge(head, reversed);
+    }
+
+    private ListNode findMid(ListNode head) {
+        ListNode prev = null;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+
+        return slow;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        return prev;
+    }
+
+    private void merge(ListNode l1, ListNode l2) {
+        while (l2 != null) {
+            ListNode next = l1.next;
+            l1.next = l2;
+            l1 = l2;
+            l2 = next;
+        }
+    }
+}
+
+/*
+
+TLDR: 3 steps, find the middle, reverse the second half, merge the two halves alternating sides
+
+TC: O(N)
+SC: O(1) - created new list nodes aka LL for the helper functions O(N + k) but not based on the N, so removing the constant k keeps me at O(1).
+
+*/
