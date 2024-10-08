e8733fa2baa362c93deded864b83a8cd99410df1
Vinh Ngo <vinhngo380@gmail.com>
2024-10-08 10:48:07 -0400

    4 (med)
    old code doesnt work for some reason, just pulled neetcode's but i knew
    the solution

--- neetcode-150/linked-lists/find_duplicate_VN.py
+++ neetcode-150/linked-lists/find_duplicate_VN.py
@@ -1,21 +1,23 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        slow, slow_temp, fast = 0, 0, nums[nums[0]]
-        while slow != fast:
+        #use a fast and slow pointer to find a cycle
+        #called floyd's algor or tortoise and hare
+        #when you find an intersection go from the start
+        #and see where it intersects again
+        #TC: O(n), at worst the slow pointer will iterate O(n) times
+        #SC: O(1), only using pointers
+        slow, fast = 0, 0
+        while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
-            
-        while slow != slow_temp:
-            slow_temp = nums[slow_temp]
-            slow = nums[slow]
-        
-        return slow
+            if slow == fast:
+                break
 
-#first solution
-# found_nums = set()
-# for num in nums:
-#     if num in found_nums:
-#         return num
-#     found_nums.add(num)
-#TC: O(n) -- finding something in a set is O(1) so at worst the duplicate is at the end
-#SC: O(n) -- the set can be O(n) b/c the duplicate is at the end
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
+
+#not really sure why my other code doesnt work tbh, but neetcode does
