cd7c9d954e3f4bacce6922d3b1afa0ffdcb79d42
Vinh Ngo <vinhngo380@gmail.com>
2024-10-20 00:02:48 -0400

    Added explanation (4 Med)

--- neetcode-150/two-pointers/two-sum-sorted-VN.py
+++ neetcode-150/two-pointers/two-sum-sorted-VN.py
@@ -1,4 +1,9 @@
 class Solution:
+    #start at each end, use the fact that the list is sorted so that you
+    #can move the left pointer to the right increase val or move right pointer
+    #to the left to decrease val
+    #TC: O(n)
+    #SC: O(1)
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left, right = 0, len(numbers) - 1
         while numbers[left] + numbers[right] != target:
